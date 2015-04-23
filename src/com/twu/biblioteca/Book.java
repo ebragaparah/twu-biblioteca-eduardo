8ecd0c0fb305ac09f5090a3e200e139a7641b313
Eduardo Braga <ebraga@thoughtworks.com>
2015-04-23 10:47:01 -0300

    added welcome message and list of books features

---
+++ src/com/twu/biblioteca/Book.java
@@ -0,0 +1,12 @@
+package com.twu.biblioteca;
+
+public class Book {
+    String name, author;
+    int yearOfPublishment;
+
+    public Book(String name, String author, int yearOfPublishment) {
+        this.name = name;
+        this.author = author;
+        this.yearOfPublishment = yearOfPublishment;
+    }
+}
