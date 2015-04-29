71b5fb0d353f37692ec858dacd7ab5f411a0d538
Eduardo Braga <ebraga@thoughtworks.com>
2015-04-29 10:26:47 -0300

    refactored book tests and movies tests, removed unnecessary methods from books and movies

--- src/com/twu/biblioteca/Library.java
+++ src/com/twu/biblioteca/Library.java
@@ -1,5 +1,6 @@
 package com.twu.biblioteca;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 
 public class Library {
@@ -58,12 +59,16 @@ public class Library {
         ArrayList<ArrayList<Object>> listOfBooksDetailed = new ArrayList<ArrayList<Object>>();
         System.out.print(" |        Name        |       Author      |       Year        |\n");
         System.out.print("  ------------------------------------------------------------\n");
-        for(Book book: books) {
-            listOfBooksDetailed.add(book.getDetails());
-            for(Object detail: book.getDetails()){
-                System.out.print("|        " + detail + "       ");
-            }
+        for(Book book : books){
+            ArrayList<Object> _book = new ArrayList<Object>();
+            System.out.print("|        " + book.getName() + "       ");
+            System.out.print("|        " + book.getAuthor() + "       ");
+            System.out.print("|        " + book.getYear() + "       ");
             System.out.print("|\n");
+            _book.add(book.getName());
+            _book.add(book.getAuthor());
+            _book.add(book.getYear());
+            listOfBooksDetailed.add(_book);
         }
         return listOfBooksDetailed;
     }
@@ -72,12 +77,18 @@ public class Library {
         ArrayList<ArrayList<Object>> listOfMoviesDetailed = new ArrayList<ArrayList<Object>>();
         System.out.print(" |        Name        |       Author      |       Year        |       Rate        |\n");
         System.out.print("  ---------------------------------------------------------------------------------\n");
-        for(Movie movie: movies) {
-            listOfMoviesDetailed.add(movie.getDetails());
-            for(Object detail: movie.getDetails()){
-                System.out.print("|        " + detail + "       ");
-            }
+        for(Movie movie : movies){
+            ArrayList<Object> _movie = new ArrayList<Object>();
+            System.out.print("|        " + movie.getName() + "       ");
+            System.out.print("|        " + movie.getDirector() + "       ");
+            System.out.print("|        " + movie.getYear() + "       ");
+            System.out.print("|        " + movie.getRate() + "       ");
             System.out.print("|\n");
+            _movie.add(movie.getName());
+            _movie.add(movie.getDirector());
+            _movie.add(movie.getYear());
+            _movie.add(movie.getRate());
+            listOfMoviesDetailed.add(_movie);
         }
         return listOfMoviesDetailed;
     }
--- test/com/twu/biblioteca/BookTest.java
+++ test/com/twu/biblioteca/BookTest.java
@@ -1,11 +1,10 @@
 package com.twu.biblioteca;
 
+import org.junit.Test;
 import org.junit.Before;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
-
-import org.junit.Test;
 
 public class BookTest {
     private Book book;
